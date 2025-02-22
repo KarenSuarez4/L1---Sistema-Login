859c4f195207bf6760940a4dbf5d8c812fbd0232
Lunna SoSa <lunna.sosa@uptc.edu.co>
2025-02-22 14:52:49 -0500

    Update index.js with routes

--- app/index.js
+++ app/index.js
@@ -8,3 +8,11 @@ console.log("Server run in port", app.get("port"));
 //Configuration
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+//Routes
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "pages", "login.html"))
+  );
+  app.get("/register", (req, res) =>
+    res.sendFile(path.join(__dirname, "pages", "register.html"))
+  );
