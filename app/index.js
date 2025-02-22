b61196292fc3c0a44a6dbfef073ef020e6e0d4b6
Lunna SoSa <lunna.sosa@uptc.edu.co>
2025-02-22 15:12:54 -0500

    Add path and fileURLToPath imports in index.js to define __dirname

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.set("port", 3000);
